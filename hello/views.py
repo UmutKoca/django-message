678e6195ee7c171b6f9384fd4f25ffb67ed83ffc
umutkoca <umutkoca@outlook.com>
2018-10-23 21:10:37 +0300

    First commit

---
+++ hello/views.py
@@ -0,0 +1,23 @@
+import re
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+	return render(request, "hello/home.html")
+
+def about(request):
+	return render(request, "hello/home.html")
+
+def contact(request):
+	return render(request, "hello/contact.html")
+
+def hello_there(request,name):	
+	return render(
+		request,
+		'hello/hello_there.html',
+		{
+			'name':name,
+			'date':datetime.now()
+		}
+	)
